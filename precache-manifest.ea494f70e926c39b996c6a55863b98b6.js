31d76c4ad89b06ad9c4a6cbaf7d4fc71911fdb2b
JamesHR <jaimehr2101@gmail.com>
2022-06-02 16:32:38 -0500

    v1.0

---
+++ precache-manifest.ea494f70e926c39b996c6a55863b98b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "458181c5c706e946b0af238e75afa69e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58667e65d951653c615a",
+    "url": "/static/css/main.f20cedb1.chunk.css"
+  },
+  {
+    "revision": "d6c806ca28fd1bd3b2d5",
+    "url": "/static/js/2.94456d00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.94456d00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58667e65d951653c615a",
+    "url": "/static/js/main.23d6a06b.chunk.js"
+  },
+  {
+    "revision": "6edef728eddcb2200382",
+    "url": "/static/js/runtime-main.ceeb224a.js"
+  }
+]);
